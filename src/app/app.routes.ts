7b06426ef4448b216088cd498bd29cec587cb64f
mythDev <az.fullstack2258@gmail.com>
2017-09-29 17:53:18 +0800

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+	{ path: 'home', component: Appcomponent }
+];
+
+export const routing = RouterModule.forRoot(routes);
