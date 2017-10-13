09e1f940d1f56f2913e115fb5c2a6e90f8b13643
mythDev <az.fullstack2258@gmail.com>
2017-10-13 18:04:53 +0800

    created login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { LoginComponent } from './login/login.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { GuideComponent } from './guide/guide.component';
 import { TodoDetailComponent } from "./todo-detail/todo-detail.component";
 import { TodoItemResolver } from './resolvers/todo-item.resolver';
 
 const routes: Routes = [
-  { path: '', component: TodoListComponent },
+  { path: '', component: LoginComponent },
 	{ path: 'aboutus', component: AboutusComponent },
-  { path: 'guide', component: GuideComponent },
+  { path: 'todo', component: TodoListComponent },
   { path: 'todo/:todo_id',
       component: TodoDetailComponent,
       resolve: {
